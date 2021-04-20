c5e728cfe4440e5343f6867b6bef7d77edaf1761
RAJHARINI-KRISHNASAMY <rajharini1997@gmail.com>
2021-04-21 00:32:03 +0530

    Add files via upload

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Encodingproject.Models;
+using Encodingproject.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Encodingproject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private IAuthenticateService _authenticateService;
+        public AuthenticationController(IAuthenticateService authenticateService)
+        {
+            _authenticateService = authenticateService;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] Admin model)
+        {
+            var Admin = _authenticateService.Authenticate(model.UserName, model.Password);
+            if (Admin == null)
+            {
+                return BadRequest(new { Message = "invalid username or password" });
+            }
+            else
+                return Ok(Admin);
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Encodingproject.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Encodingproject.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ShopContext _context;
+        public ProductController(ShopContext context)
+        {
+            _context = context;
+
+            _context.Database.EnsureCreated();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts() {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet,Route("/Product/{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
