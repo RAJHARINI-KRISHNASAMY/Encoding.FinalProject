dce50986d19d8844cfcf8a3b81a9d69432033200
RAJHARINI-KRISHNASAMY <rajharini1997@gmail.com>
2021-04-21 00:33:35 +0530

    Add files via upload

---
+++ Services/IAuthenticateService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Encodingproject.Models;
+
+namespace Encodingproject.Services
+{
+    public interface IAuthenticateService
+    {
+        Admin Authenticate(String UserName, String Password);
+    }
+}
